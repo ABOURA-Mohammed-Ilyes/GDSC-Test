ca0fdabb7360ea9635446fefc154a4fd53e60170
ABOURA Mohammed Ilyes <abouramohammedilyes@gmail.com>
2023-11-27 17:16:36 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import About from './components/About';
 import Events from './components/Events';
@@ -11,35 +11,39 @@ import UsersDisplay from './components/UsersDisplay';
 import GoogleLoginComponent from './components/GoogleLoginComponent';
 import UserInfo from './components/user-info';
 
-export const ThemeContext  = createContext("")
+export const ThemeContext  = createContext("");
+
 function App() {
   const [dark, setdark] = useState("");
-  const  dartAction = () =>{
-    setdark(!dark)
-  }
+  const dartAction = () => {
+    setdark(!dark);
+  };
+
   return (
     <BrowserRouter>
-        <ThemeContext.Provider value={dark}>
-        <div style={{ backgroundColor : dark ? "black" : "", color: dark ? "white" : ""  }} >
-        <div style={{ backgroundColor : dark ? "black" : "", color: dark ? "white" : ""  }} className="App">
-          <Navbar />
-          <button className='border-0 bg-transparent' onClick={dartAction} >
-          <i style={{ fontSize:"30px", zIndex:"4", left:34 }} class="fa-solid fa-circle-half-stroke position-fixed top-50 "></i>
-          </button>
-          <Header/>
-          <About />
-          <Events />
-          <Organizer />
-          <hr ></hr>
-          <Footer />
+      <ThemeContext.Provider value={dark}>
+        <div style={{ backgroundColor: dark ? "black" : "", color: dark ? "white" : "" }}>
+          <div style={{ backgroundColor: dark ? "black" : "", color: dark ? "white" : "" }} className="App">
+            <Navbar />
+            <button className='border-0 bg-transparent' onClick={dartAction}>
+              <i style={{ fontSize: "30px", zIndex: "4", left: 34 }} className="fa-solid fa-circle-half-stroke position-fixed top-50"></i>
+            </button>
+            <Header/>
+            <Switch>
+              <Route exact path="/" component={About} />
+              <Route path="/events" component={Events} />
+              <Route path="/organizer" component={Organizer} />
+              <Route path="/users" component={UsersDisplay} />
+              <Route path="/login" component={GoogleLoginComponent} />
+              <Route path="/user-info" component={UserInfo} />
+              {/* Ajoutez d'autres routes si nécessaire */}
+            </Switch>
+            <hr></hr>
+            <Footer />
+          </div>
         </div>
-        </div>
-        </ThemeContext.Provider>
-        <UsersDisplay />
-        <GoogleLoginComponent />
-        <UserInfo />
+      </ThemeContext.Provider>
     </BrowserRouter>
-
   );
 }
 
