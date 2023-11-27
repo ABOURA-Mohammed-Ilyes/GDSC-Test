632351688bc574a9de2eb5756f3e02639640dfda
ABOURA Mohammed Ilyes <abouramohammedilyes@gmail.com>
2023-11-27 19:12:45 +0100

    set back app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header';
 import About from './components/About';
 import Events from './components/Events';
@@ -11,39 +11,35 @@ import UsersDisplay from './components/UsersDisplay';
 import GoogleLoginComponent from './components/GoogleLoginComponent';
 import UserInfo from './components/user-info';
 
-export const ThemeContext  = createContext("");
-
+export const ThemeContext  = createContext("")
 function App() {
   const [dark, setdark] = useState("");
-  const dartAction = () => {
-    setdark(!dark);
-  };
-
+  const  dartAction = () =>{
+    setdark(!dark)
+  }
   return (
     <BrowserRouter>
-      <ThemeContext.Provider value={dark}>
-        <div style={{ backgroundColor: dark ? "black" : "", color: dark ? "white" : "" }}>
-          <div style={{ backgroundColor: dark ? "black" : "", color: dark ? "white" : "" }} className="App">
-            <Navbar />
-            <button className='border-0 bg-transparent' onClick={dartAction}>
-              <i style={{ fontSize: "30px", zIndex: "4", left: 34 }} className="fa-solid fa-circle-half-stroke position-fixed top-50"></i>
-            </button>
-            <Header/>
-            <Routes>
-              <Route exact path="/" component={About} />
-              <Route path="/events" component={Events} />
-              <Route path="/organizer" component={Organizer} />
-              <Route path="/users" component={UsersDisplay} />
-              <Route path="/login" component={GoogleLoginComponent} />
-              <Route path="/user-info" component={UserInfo} />
-              {/* Ajoutez d'autres routes si nécessaire */}
-            </Routes>
-            <hr></hr>
-            <Footer />
-          </div>
+        <ThemeContext.Provider value={dark}>
+        <div style={{ backgroundColor : dark ? "black" : "", color: dark ? "white" : ""  }} >
+        <div style={{ backgroundColor : dark ? "black" : "", color: dark ? "white" : ""  }} className="App">
+          <Navbar />
+          <button className='border-0 bg-transparent' onClick={dartAction} >
+          <i style={{ fontSize:"30px", zIndex:"4", left:34 }} class="fa-solid fa-circle-half-stroke position-fixed top-50 "></i>
+          </button>
+          <Header/>
+          <About />
+          <Events />
+          <Organizer />
+          <hr ></hr>
+          <Footer />
         </div>
-      </ThemeContext.Provider>
+        </div>
+        </ThemeContext.Provider>
+        <UsersDisplay />
+        <GoogleLoginComponent />
+        <UserInfo />
     </BrowserRouter>
+
   );
 }
 
