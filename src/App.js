bf64c180a1dba7c9166a5bf03eafd82596db247c
ABOURA Mohammed Ilyes <abouramohammedilyes@gmail.com>
2023-12-01 20:47:30 +0100

    Added Eden changes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Header from './components/Header';
 import About from './components/About';
 import Events from './components/Events';
 import Organizer from './components/Organizer';
 import Footer from './components/Footer';
-import { createContext, useState } from 'react';
+import React, { useState } from 'react'
+import { createContext } from 'react';
 import UsersDisplay from './components/UsersDisplay';
 import GoogleLoginComponent from './components/GoogleLoginComponent';
 import UserInfo from './components/user-info';
 
 export const ThemeContext  = createContext("")
-function App() {
-  const [dark, setdark] = useState("");
-  const  dartAction = () =>{
-    setdark(!dark)
-  }
+
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Callback function to update darkMode state
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };   
+
   return (
     <BrowserRouter>
-        <ThemeContext.Provider value={dark}>
-        <div style={{ backgroundColor : dark ? "black" : "", color: dark ? "white" : ""  }} >
-        <div style={{ backgroundColor : dark ? "black" : "", color: dark ? "white" : ""  }} className="App">
-          <Navbar />
-          <button className='border-0 bg-transparent' onClick={dartAction} >
-          <i style={{ fontSize:"30px", zIndex:"4", left:34 }} class="fa-solid fa-circle-half-stroke position-fixed top-50 "></i>
-          </button>
+        <ThemeContext.Provider value={darkMode}>
+        <div style={{ backgroundColor : darkMode ? "#1a1a1a" : "", color: darkMode ? "white" : ""  }} >
+        <div style={{ backgroundColor : darkMode ? "#1a1a1a" : "", color: darkMode ? "white" : ""  }} className="App">
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} style={{ backgroundColor : darkMode ? "#1a1a1a" : "", color: darkMode ? "white" : ""  }}/>
+          {/*<button className='border-0 bg-transparent' onClick={dartAction} >
+          <i style={{ fontSize:"30px", zIndex:"4", left:34,color: darkMode ? "white" : ""   }} class="fa-solid fa-circle-half-stroke position-fixed top-50 "></i>
+  </button>*/}
           <Header/>
           <About />
           <Events />
@@ -35,7 +41,7 @@ function App() {
         </div>
         </div>
         </ThemeContext.Provider>
-        <UsersDisplay />
+        {/* {<UsersDisplay />} */}
         <GoogleLoginComponent />
         <Routes>
           <Route path="/user-info" element={<UserInfo />} />
